dbbc948f13d7f982a3bfbb4e50445c332038e6e9
Administrator <1058491659@qq.com>
2016-06-29 10:55:17 +0800

    6.29.10:53

--- js/main.js
+++ js/main.js
@@ -1,13 +1,13 @@
- $(function() {
-     $(window).scroll(function(){
-      if ($(window).scrollTop()>385){
-        $("#fix-box").addClass('active');
-      }
-      else
-      {
-       $("#fix-box").removeClass('active');
-      }
-      });
 
+$(function(){
+    var tab=$("#g-bannerLast .g-lastBtn");
+    var contentShow=$("#g-content .g-contentMain");
+    tab.each(function(i){
+        $(this).on("mouseover",function(){
+            $(this).addClass("g-active");
+            $(this).siblings().removeClass("g-active");
+            contentShow.eq(i).show();
+            contentShow.eq(i).siblings().hide();
+        })
+    })
 });
-
