8d8048e3a786c6b9846329f466ef72efee602ec6
Administrator <1058491659@qq.com>
2016-06-30 16:02:42 +0800

    6.30.16:00

--- js/main.js
+++ js/main.js
@@ -11,3 +11,38 @@ $(function(){
         })
     })
 });
+
+$(function(){
+    var signhead=$(".g-signIndexHead .g-signChoose");
+    var signheadshow=$(".g-stepMain .signheadshow");
+    signhead.each(function(i){
+        $(this).on("click",function(){
+            $(this).addClass("g-signChooseActive");
+            $(this).siblings().removeClass("g-signChooseActive");
+            signheadshow.eq(i).show();
+            signheadshow.eq(i).siblings().hide();
+        })
+    })
+});
+
+$(function(){
+    var signCodesBtn=$("#g-signCodesBtn");
+    var c=60;
+    var t;
+    signCodesBtn.on("click",function(){
+        timedCount();
+    });
+    function timedCount()
+    {
+        signCodesBtn.attr("disabled","true");
+        signCodesBtn.val(c+"s");
+        c--;
+        t=setTimeout(timedCount,1000);
+        if(c<0){
+            clearTimeout(t);
+            signCodesBtn.val("重试");
+            signCodesBtn.removeAttr("disabled");
+            c=60;
+        }
+    }
+});
